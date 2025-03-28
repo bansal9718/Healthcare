b040503219dfcfbbd662425457420d9013c69a45
Rohit <rohitbansal4685@gmail.com>
2025-03-28 22:16:09 +0530

    Fix

--- Appointment-App/backend/app.js
+++ Appointment-App/backend/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://advancedcardiaccare.netlify.app/",
+    origin: "https://advancedcardiaccare.netlify.app",
     credentials: true,
   })
 );
